d13904bd0778be48dea52894d8d8ef6941bc8021
KamilKerso <kamilbaran048@gmail.com>
2021-10-21 14:27:19 +0200

    Add project files.

---
+++ Prime_FINAL3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstNPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Witaj w programie z liczbami pierwszymi! \nOkreśl ile liczb pierwszych chcesz uzyskać: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Oto Twoje liczb pierwsze:");
+            int PrimeCount = 0, count, num = 2;
+
+            while (PrimeCount < n)
+            {
+                count = 0;
+                for (int i = 2; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                    }
+                    if (count >= 2)
+                    {
+                        break;
+                    }
+                }
+                if (count < 2)
+                {
+                    PrimeCount++;
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+            Console.WriteLine("Naciśnij jakikolwiek klawisz by zakończyć działanie programu");
+            Console.ReadKey();
+        }
+    }
+}
